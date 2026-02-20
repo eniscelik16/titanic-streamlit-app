f5e35ee09d61255f97661b7070ae428da79000ce
eniscelik16 <enisccelik@gmail.com>
2026-02-20 15:55:32 +0300

    Last updated

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import plotly.express as px
 import streamlit as st
 import pandas as pd
 
 st.set_page_config(layout='wide')
 st.title('Titanic Dashboard')
 
-df = pd.read_csv('titanic_data.csv')
+df = pd.read_csv('titani_data.csv')
 # st.write(df)
 
 # print(df.info())
@@ -23,3 +24,40 @@ selected_gender = col2.selectbox(options=gender,
 # Filter dataframe based on the widget values
 df_plot = df[df['Embarked'] == selected_port]
 df_plot = df_plot[df_plot['Sex'] == selected_gender]
+
+hist_plot = px.histogram(data_frame=df,
+                    template='seaborn',
+                    color='Survived',
+                    title='Distribution of Age',
+                    facet_col='Survived',
+                    x='Age')
+
+col1.plotly_chart(hist_plot)
+
+# add a pie chart
+# Create the pie chart using plotly express
+df_plot_pie = df_plot.loc[:, ['PassengerId','Survived']].groupby(['Survived']).count().reset_index()
+df_plot_pie.rename({'PassengerId': 'Count of passengers'},
+                   axis='columns',
+                   inplace=True)
+
+pie_plot = px.pie(data_frame=df_plot_pie,
+                  template='seaborn',
+                  title='Count of passengers that survived',
+                  values='Count of passengers',
+                  names='Survived')
+
+# attach to the dashboard
+col2.plotly_chart(pie_plot)
+
+# add a boxplot of the fare prices
+# wrangle the data
+# create the visual
+box_plot = px.box(data_frame=df_plot,
+              y='Fare',
+              template='seaborn',
+              color='Survived',
+              title='Distribution of fare across survival status',
+              x='Survived')
+# attach to the dashboard
+st.plotly_chart(box_plot)
